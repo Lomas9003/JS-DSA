8d134e6638ba87fd7119cdf52a3d13b06659341f
Ishan Lomas <ishanlomas998@gmail.com>
2025-05-08 16:58:37 +0530

    capital

---
+++ Capital.js
@@ -0,0 +1,25 @@
+function capitalizeWords(sentence) {
+    // Your implementation  
+    let val = sentence.trim().split(/\s+/).join(' ')
+    let result = ""; 
+    let capital = true  
+
+    for (let i = 0; i < val.length; i++){
+        let ch = val[i];
+
+        if (ch === " ") { 
+            result = result + ch;
+            capital = true; 
+        } else {
+            if (capital && ch >= 'a' && ch <= 'z') {
+                result = result + val[i].toUpperCase()
+            } else if (!capital && ch >= 'A' && ch <= 'Z') {
+                result = result + val[i].toLowerCase()
+            } else {
+                result = result + ch;
+            }
+            capital = false
+        }
+    }
+    return result;
+}
