27b928ca0aefdf91a2ad0bab138ba62b64d26633
Ishan Lomas <ishanlomas998@gmail.com>
2025-05-08 18:36:19 +0530

    secondlargest

--- SecondLargest.js
+++ SecondLargest.js
@@ -1,5 +1,24 @@
 //  In this we have to find the second largest in array 
 
+function second(arr){
+    if(arr.length < 2) return null;  
+
+    let first = -Infinity
+    let second = -Infinity; 
+    for(let num of arr){
+        if(num > first){
+            second = first; 
+            first = num
+        }else if (num > second && num < first){
+            second = num
+        }
+    }
+    return second === -Infinity ? null : second
+}
+
+let arr1 = [2,5,3,11,11,10]
+console.log(second(arr1))
+
 function secondLargest(arr){
     let val = [...new Set(arr)] 
     
