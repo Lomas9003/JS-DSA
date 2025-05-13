4bced87f9a1fab6d57df4d4ff82c3a00fc4e56a0
Ishan Lomas <ishanlomas998@gmail.com>
2025-05-13 23:21:06 +0530

    Memoized

--- memoized.js
+++ memoized.js
@@ -14,3 +14,34 @@ function sum1(){
     }
 
 }
+
+// advance memoized 
+
+function memoize(fn) {
+    // write code here 
+    let cache = new Map()
+
+    return function (...args) {
+        const key = args.map((arg) => {
+            if (typeof arg === 'object' && arg !== null) {
+                return JSON.stringify(Object.entries(arg).sort());
+            }
+            return JSON.stringify(arg)
+        }).join('|')
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        const result = fn(...args)
+        cache.set(key, result)
+        return result
+    }
+}
+
+function sub(a ,b){
+    return a - b
+}
+
+const memoizeSub = memoize(sub)
+memoizeSub(3,2)
