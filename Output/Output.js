891df53a8a8aa6179668b65f12889039d51ae531
Ishan Lomas <ishanlomas998@gmail.com>
2025-05-15 17:18:30 +0530

    output

---
+++ Output/Output.js
@@ -0,0 +1,8 @@
+console.log(('a' + 'a' + + 'b' + 'a').toLowerCase()); // aanana
+console.log(+ "b") // + tries to convert b in a number but fails so give NAN
+console.log(0.1*1 == 0.1) // true
+console.log(0.1*2 == 0.2) //true
+console.log(0.1*3 == 0.3) //false // 0.1 is a float value that represent in binary in js 
+console.log(0.1*4 == 0.4) //true
+console.log(0.1*5 == 0.5) //true
+console.log(0.1*9 == 0.9) //true
