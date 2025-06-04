a3996dea8753211c4ff61556ce1a8306d7befcaf
Ishan Lomas <ishanlomas998@gmail.com>
2025-06-04 18:07:44 +0530

    updated

--- LengthOFLongestStr.js
+++ LengthOFLongestStr.js
@@ -6,7 +6,8 @@ function lengthOfLongestSubstring(s) {
 
     for (let i = 0; i < s.length; i++){
         while (result.has(s[i])) {
-            result.delete(s[i])
+           // result.delete(s[i]) /// it will give the longest subString like pwwkew => pwke - 4
+            result.delete(s[left]) // it will give contiiues substring like pwwkew => wke - 3
             left++
         }
         result.add(s[i])
