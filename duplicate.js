7c0b0c53ff0a048e114dbe1190359307fde2e0a8
Ishan Lomas <ishanlomas998@gmail.com>
2025-07-10 21:54:40 +0530

    feature

--- duplicate.js
+++ duplicate.js
@@ -11,6 +11,28 @@ const remove = removeDuplicate([1,1,2,2,3,3,4,4,5,5,false, false])
 // console.log(remove);
 
 
+// without index of 
+
+function removeDuplicate(arr){
+    let result = {}
+    let val = []
+    
+    for(let i =0; i<arr.length ; i++){
+        if(!result[arr[i]]){
+            val.push(arr[i])
+            result[arr[i]] = true
+        }
+    }
+    return val
+}
+
+
+
+
+// const remove = removeDuplicate([1,1,2,2,3,3,4,4,5,5,false, false])
+// console.log(remove);
+
+
 // if we want the count of uniqe elements in an array 
 
 function uniqe(){
--- findLargestInNestedArray.js
+++ findLargestInNestedArray.js
@@ -23,6 +23,41 @@ const nestedArr = [[10, 25, 30], 120,  [5, 100, 2], [7, 20, 50] , 16];
 // console.log(findLargestInNestedArray(nestedArr)); 
 
 
+// find the second largest in nested 
+
+function findLargestInNestedArray(arr){
+    let max = -Infinity 
+    let second = -Infinity
+    
+    for(let val of arr){
+        if(Array.isArray(val)){
+            for(let val2 of val){
+                if(val2 > max){
+                    second = max
+                    max = val2
+                }else if(val2 > second && val2 !== max){
+                    second = val2
+                }
+            }
+        }else{
+            if(val > max){
+                second = max
+                max = val
+            }else if(val > second && val !== max){
+                second = val
+            } 
+        }
+    }
+    return second
+}
+
+
+
+
+// const nestedArr = [[10, 25, 30], 120,  [5,150,300 , 100, 2], [7,300, 20, 50] , 16];
+//  console.log(findLargestInNestedArray(nestedArr)); 
+
+
 // flaten the nested array 
 function flattenArray(arr){
     let stack = [...arr]
