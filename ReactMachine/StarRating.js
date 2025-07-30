ca10bf0a2a9848ad8cd071712253a4f4bee747fb
Ishan Lomas <ishanlomas998@gmail.com>
2025-07-30 10:17:13 +0530

    code

--- ReactMachine/StarRating.js
+++ ReactMachine/StarRating.js
@@ -1,38 +1,55 @@
-import { useState } from "react";
-import "./styles.css";
+import React, { useState } from 'react'
 
-export default function App() {
-  const [rating, setRating] = useState(0);
-  const [hover, setHover] = useState(null);
-  const val = new Array(5).fill(0);
+const StarRatting = () => {
+  const [rating, setRating] = useState(0)
+  const [hover, setHover] = useState(0)
 
+    const handleClick = (index) => {
+        const clickedStar = index + 1
+        if(clickedStar === rating){
+            setRating(rating - 1)
+        }else{
+                setRating(index + 1)
+
+        }
+  }
+
+  const handleMouseEnter = (index) => {
+    setHover(index + 1)
+  }
+
+  const handleMouseLeave = () => {
+    setHover(0)
+  }
   return (
-    <div style={{ textAlign: "center", padding: "20px" }}>
-      <h1>Star Rating</h1>
-      <h3>by NamasteDev</h3>
-
-      {/* Step 2: Render 5 stars using a loop ★★★★★ */}
-      {val.map((star, index) => (
-        <span
-          onMouseEnter={() => setHover(index + 1)}
-          onMouseOut={() => setHover(0)}
-          onClick={() => setRating(index + 1)}
-          style={{
-            fontSize: "70px",
-            color: index < (rating || hover) ? "yellow" : "black",
-          }}
-        >
-          ★
-        </span>
-      ))}
-      {/* Step 3: Update rating when a star is clicked */}
-      <h3>Current Rating: {rating}</h3>
-      {/* Step 4: Style stars based on rating */}
-
-      {/* Step 5: Display current rating */}
-
-      {/* Step 6: Add a Reset button to clear the rating */}
-      <button onClick={() => setRating(null)}>Reset Rating</button>
+    <div>
+        <h1>Star Ratting</h1>
+        <div className='star-container'>
+            <div>
+           {[1, 2, 3, 4, 5].map((val, index) => (
+          <span
+            key={index}
+            className='star'
+            style={{
+                fontSize: '60px',
+              color: index < (hover || rating) ? 'yellow' : 'black',
+              cursor: 'pointer',
+             
+            }}
+            onClick={() => handleClick(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={handleMouseLeave}
+          >
+            ★
+          </span>
+        ))}
+        </div>
+        <div>
+            <h3>Ratting: {rating}</h3>
+        </div>
+        </div>
     </div>
-  );
+  )
 }
+
+export default StarRatting
